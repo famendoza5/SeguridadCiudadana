bb3c29d4d2bb0e3c119ebbb5eb5e2b3bf41e7184
fmendoza <fmendoza@C-DESLLO22>
2022-06-07 21:17:35 -0500

    Agregar archivos de proyecto.

---
+++ SeguridadCiudadana/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SeguridadCiudadana.MvcApplication" Language="C#" %>
---
+++ SeguridadCiudadana/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeguridadCiudadana.Startup))]
+namespace SeguridadCiudadana
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
